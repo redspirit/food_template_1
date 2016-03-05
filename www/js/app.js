aa0bd2ee88137a344eaba15a8b81b8466b5a6a02
Alexey <redspirit@live.ru>
2016-03-06 01:18:26 +0300

    load data and make days grid

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,25 @@
 var app = angular.module('Template', []);
 
-app.controller('MainController', function($scope){
+app.controller('MainController', function($scope, $http){
 
+    /* load data */
+    $scope.items = [];
+    $http.get("data/items.json").then(function(res){
+        $scope.items = res.data.items;
+        console.log("items data", $scope.items);
+    });
 
-    $scope.testText = "hello world";
+    /* make days grid */
+    moment.locale('ru');
+    $scope.timeGrid = [];
+    for(var i = 0; i < 7; i++) {
+        var d = moment().add(i, 'days');
+        $scope.timeGrid.push({
+            day: d.format('DD'),
+            month: d.format('MMMM'),
+            today: i == 0
+        });
+    }
 
 
 
